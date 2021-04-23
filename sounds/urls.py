5cd981773993176a0754a7565beeb137c233a88b
Ali <sadrali1990@gmail.com>
2021-04-24 00:59:36 +0300

    Made static background, upgraged some classes in view.py, upgraded htmls little bit

--- sounds/urls.py
+++ sounds/urls.py
@@ -3,12 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
-    path('songs/', SongsList.as_view(), name='songlist'),
+    path('search/', Music.as_view(), name='search'),
     path('playlists/', Playlists.as_view(), name='playlists'),
     path('download/', DownloadApp.as_view(), name='downloadapp'),
     path('', TheSong.as_view(), name='thesong'),
     path('', TheArtist.as_view(), name='theartist'),
-    path('', ArtistsList.as_view(), name='artistslist'),
     path('', ThePlaylist.as_view(), name='theplaylist'),
-    path('', LoginPage.as_view(), name='loginpage'),
+    path('login/', LoginPage.as_view(), name='loginpage'),
 ]
--- sounds/views.py
+++ sounds/views.py
@@ -1,29 +1,46 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 
 from .models import *
 
 
+menu = [
+        {"title": "Главная", "url_name": "home"},
+        {"title": "Музыка", "url_name": "music"},
+        {"title": "Скачать", "url_name": "downloadapp"},
+        {"title": "Плейлисты", "url_name": "playlists"},
+        ]
+
 class HomePage(ListView):
-    pass
-
-
-class SongsList(ListView):
     model = Song
-    template_name = 'sounds/base.html'
+    template_name = 'sounds/index.html'
+    extra_context = {'title': 'Главная страница'}
 
 
-class ArtistsList(ListView):
-    pass
+class Music(ListView):
+    model = Song
+    template_name = 'sounds/music.html'
 
 
 class Playlists(ListView):
-    pass
+    paginate_by = 3
+    posts = Playlist
+    template_name = 'advertising/internet.html'
+    context_object_name = 'posts'
+    extra_context = {'title': 'Реклама в интернете'}
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        return context
+
+    def get_queryset(self):
+        return Playlist.objects.filter()
 
 
-class DownloadApp(ListView):
-    pass
+class DownloadApp(TemplateView):
+    template_name = 'sounds/download_app.html'
 
 
 class TheSong(ListView):
@@ -36,7 +53,7 @@ class TheArtist(ListView):
 class ThePlaylist(ListView):
     pass
 
-class LoginPage(ListView):
-    pass
-
+class LoginPage(TemplateView):
+    template_name = 'sounds/login_page.html'
+    extra_context = {'title': 'Авторизация'}
 
